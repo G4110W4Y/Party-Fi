4e2299518643285949419ab69c0dd209f875d4fc
Connor L <wowclambert0710@gmail.com>
2017-11-08 18:44:45 -0600

    Added using/Imports

--- PartyFi/PartyFi/Models/Song.cs
+++ PartyFi/PartyFi/Models/Song.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
